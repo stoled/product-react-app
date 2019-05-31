1e8d8af3e742c0e272fbdfef7873eef15c753d5a
GrigoryCode <grigorycode@gmail.com>
2019-05-31 16:10:44 +0300

    Add navbar

--- src/App.js
+++ src/App.js
@@ -6,18 +6,24 @@ import { Home } from './pages/Home';
 import { About } from './pages/About';
 import { Catalog } from './pages/Catalog';
 import { NoMatch } from './pages/NoMatch';
+import { Layout } from './components/Layout';
+import { NavBar } from './components/NavBar';
+
 
 function App() {
   return (
     <React.Fragment>
-      <Router>
-        <Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/about" component={About} />
-          <Route path="/catalog" component={Catalog} />
-          <Route component={NoMatch} />
-        </Switch>
-      </Router>
+      <NavBar />
+      <Layout>
+        <Router>
+          <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/catalog" component={Catalog} />
+            <Route component={NoMatch} />
+          </Switch>
+        </Router>
+      </Layout>
     </React.Fragment>
   );
 }
